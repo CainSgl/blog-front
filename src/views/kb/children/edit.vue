8e9c83b4e0d6255ec613125726c9a3dd9910d595
cainsgl <18523785718@163.com>
2025-12-19 17:17:52 +0800

    edit

--- src/views/kb/children/edit.vue
+++ src/views/kb/children/edit.vue
@@ -1,11 +1,36 @@
 <template>
- 
+  <div class="editor-container">
+    <v-md-editor
+      v-model="text"
+      height="100vh"
+      :disabled-menus="[]"
+      @upload-image="handleUploadImage"
+    ></v-md-editor>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const text = ref('');
+
+const handleUploadImage = (event, insertImage, files) => {
+    console.log(files[0].name)
+  
+  const url = 'https://example.com/image.jpg';
+  insertImage({
+     url: files[0].name,
+     desc: '图片描述',
+     width: 'auto',
+     height: 'auto',
+  });
+};
 </script>
 
 <style scoped lang="less">
-
+.editor-container {
+  background-color: #fff;
+  border-radius: 4px;
+  height: 100vh;
+}
 </style>
