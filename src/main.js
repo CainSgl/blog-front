8512f7f94abaff62a96e490fece6e03c07240e40
cainsgl <18523785718@163.com>
2025-12-19 17:19:33 +0800

    edit

--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ import router from './router';
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
 import './assets/style/global.less';
-
+import VMdEditor from '@kangc/v-md-editor';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
 app.use(ArcoVue);
+app.use(VMdEditor);
 app.config.errorHandler = (err, instance, info) => {
   console.error('全局异常:', err, '组件:', instance, '位置:', info);
 };
